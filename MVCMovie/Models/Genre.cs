b97077ee640bc41612e3290af9bba8e94ca042bf
Diego Rios Vasquez <d.rios@hkgdutyfree.com>
2023-05-04 19:09:09 -0500

    Models Added

---
+++ MVCMovie/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCMovie.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int IdGenre { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ MVCMovie/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int IdMovie { get; set; }
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+        [StringLength(100)]
+        public string Genre { get; set; } = string.Empty;
+        public int Year { get; set; }
+    }
+}
